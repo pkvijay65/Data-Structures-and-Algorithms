1ea432adf209ec4ca28ea041d15e2cc4a3979558
Prateek Vijay <113239008+pkvijay65@users.noreply.github.com>
2022-10-17 15:11:00 +0530

    Time: 5 ms (37.22%), Space: 42.3 MB (35.65%) - LeetHub

---
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+        HashSet<Character> hs = new HashSet<Character>();
+        
+        for (int i = 0; i<sentence.length(); i++){
+            hs.add(sentence.charAt(i));
+        }
+        
+        for (int i = 0; i<26; i++){
+            char ch = (char)('a' + i);
+            if (!hs.contains(ch)){
+                return false;
+            }
+        }
+        return true;
+        
+    }
+}
