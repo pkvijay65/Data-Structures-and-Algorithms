a8e1aee672abb43f2629837aa8dc88e0d0c37d69
Prateek Vijay <113239008+pkvijay65@users.noreply.github.com>
2022-10-29 00:03:40 +0530

    Time: 87 ms (9.31%), Space: 74 MB (5.10%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,36 +1,36 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> result = new ArrayList<>();
-        if (strs == null || strs.length == 0) {
-            return result;
-        }
-        if (strs.length == 1) {
-            result.add(Arrays.asList(strs));
-            return result;
-        }
-
-        HashMap<String, List<String>> groups = new HashMap<>();
-        for (String s : strs) {
-            String signature = getSignature(s);
-            groups.putIfAbsent(signature, new ArrayList<>());
-        groups.get(signature).add(s);
-        }
-
-        return new ArrayList<>(groups.values());
-    }
-
-    private String getSignature(String s) {
-        int[] count = new int[26];
-        for (int i = 0; i < s.length(); i++) {
-            count[s.charAt(i) - 'a']++;
-        }
-
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < 26; i++) {
-            if (count[i] != 0) {
-                sb.append((char) ('a' + i)).append(count[i]);
+        
+        HashMap<HashMap<Character, Integer>, ArrayList<String>> bmap = new HashMap<>();
+        
+        for (String str : strs){
+            HashMap<Character, Integer> fmap = new HashMap<>();
+            
+            for (int i = 0; i< str.length(); i++){
+                char ch = str.charAt(i);
+                fmap.put(ch, fmap.getOrDefault(ch, 0) + 1);
             }
+            
+            if (!bmap.containsKey(fmap)){
+                 ArrayList<String> list = new ArrayList<>();
+                list.add(str);
+                bmap.put(fmap, list);
+            }
+            else {
+                ArrayList<String> list = bmap.get(fmap);
+                list.add(str);
+            }
+            
         }
-        return sb.toString();
+        
+        List<List<String>> ans = new ArrayList<>();
+        
+        for (ArrayList<String> str : bmap.values()){
+            ans.add(str);
+        }
+        
+        return ans;
+       
+        
     }
 }
