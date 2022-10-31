0ae5e74b5ea5956e12641d93b9fb62c96ad93d13
Prateek Vijay <113239008+pkvijay65@users.noreply.github.com>
2022-10-31 12:13:36 +0530

    Time: 1077 ms (6.87%), Space: 0B (100.00%) - LeetHub

--- 0177-nth-highest-salary/0177-nth-highest-salary.sql
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -3,6 +3,8 @@ BEGIN
 SET N = N - 1;
   RETURN (
       # Write your MySQL query statement below.
-      SELECT DISTINCT salary from Employee order by salary desc limit 1 offset N
+      # SELECT DISTINCT salary from Employee order by salary desc limit 1 offset N
+      
+      select DISTINCT salary from Employee as e1 where N = (select count(DISTINCT salary) from Employee as e2 where e2.salary > e1.salary )
   );
 END
