87618e6e11722958458f03c2b381f5e81ce4a710
Prateek Vijay <113239008+pkvijay65@users.noreply.github.com>
2023-03-13 17:08:43 +0530

    Time: 1 ms (100.00%), Space: 43.4 MB (23.71%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -6,36 +6,52 @@ class Solution {
             return 0;
         }
         
-        int[] prefixSum = new int[len];
-        int[] suffixSum = new int[len];
-        
+        int leftSum = 0; //sum till ith index element excluding ith index element
+        int totalSum = 0;
+
+        for (int num : nums) {
+            totalSum += num;
+        }
         
         for (int i = 0; i < len; i++) {
-            if (i == 0) {
-                prefixSum[i] = nums[i];
-            } else prefixSum[i] = prefixSum[i-1] + nums[i];
-        }
-        
-        for (int i = len - 1; i >= 0; i--) {
-            if (i == len - 1) {
-                suffixSum[i] = nums[i];
-            } else suffixSum[i] = suffixSum[i+1] + nums[i];
-        }
-        
-        for (int i = 0; i <= len - 1; i++) {
-            if (i == 0) {
-                if (suffixSum[i+1] == 0) {
-                    return i;
-                }
-            } else if (i == len - 1) {
-                if (prefixSum[len -2] == 0) {
-                    return i;
-                }
-            } else if (prefixSum[i - 1] == suffixSum[i + 1]) {
+            if (2 * leftSum == totalSum - nums[i]) {
                 return i;
             }
+            
+            leftSum += nums[i];
         }
         
         return -1;
+//         int[] prefixSum = new int[len];
+//         int[] suffixSum = new int[len];
+        
+        
+//         for (int i = 0; i < len; i++) {
+//             if (i == 0) {
+//                 prefixSum[i] = nums[i];
+//             } else prefixSum[i] = prefixSum[i-1] + nums[i];
+//         }
+        
+//         for (int i = len - 1; i >= 0; i--) {
+//             if (i == len - 1) {
+//                 suffixSum[i] = nums[i];
+//             } else suffixSum[i] = suffixSum[i+1] + nums[i];
+//         }
+        
+//         for (int i = 0; i <= len - 1; i++) {
+//             if (i == 0) {
+//                 if (suffixSum[i+1] == 0) {
+//                     return i;
+//                 }
+//             } else if (i == len - 1) {
+//                 if (prefixSum[len -2] == 0) {
+//                     return i;
+//                 }
+//             } else if (prefixSum[i - 1] == suffixSum[i + 1]) {
+//                 return i;
+//             }
+//         }
+        
+//         return -1;
     }
 }
