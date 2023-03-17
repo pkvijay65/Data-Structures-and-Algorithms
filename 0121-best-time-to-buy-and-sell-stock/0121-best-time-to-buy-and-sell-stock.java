d170a10d891f243c860f2e8b1e293d02b02cdc6c
Prateek Vijay <113239008+pkvijay65@users.noreply.github.com>
2023-03-17 10:53:57 +0530

    Time: 2 ms (76.59%), Space: 59.4 MB (22.63%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,31 +1,38 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int len = prices.length;
-        
-        int[] prefixMin = new int[len];
+        int maxProfit = 0;
+        int minPrice = Integer.MAX_VALUE;
         
         for (int i = 0; i < len; i++) {
-            if (i == 0) {
-                prefixMin[i] = prices[i];
-            }
-            else prefixMin[i] = Math.min(prices[i], prefixMin[i-1]);
+            minPrice = Math.min(minPrice, prices[i]);
+            maxProfit = Math.max(maxProfit, prices[i] - minPrice);
         }
-        
-        int[] suffixMax = new int[len];
-        
-        for (int i = len - 1; i >= 0; i--) {
-            if (i == len - 1) {
-                suffixMax[i] = prices[i];
-            }
-            else suffixMax[i] = Math.max(prices[i], suffixMax[i+1]);
-        }
-        
-        int maxProfit = 0;
-        
-        for (int i = 0; i < len ; i++) {
-            maxProfit = Math.max(maxProfit, suffixMax[i] - prefixMin[i]);
-        }
-        
         return maxProfit;
+//         int[] prefixMin = new int[len];
+        
+//         for (int i = 0; i < len; i++) {
+//             if (i == 0) {
+//                 prefixMin[i] = prices[i];
+//             }
+//             else prefixMin[i] = Math.min(prices[i], prefixMin[i-1]);
+//         }
+        
+//         int[] suffixMax = new int[len];
+        
+//         for (int i = len - 1; i >= 0; i--) {
+//             if (i == len - 1) {
+//                 suffixMax[i] = prices[i];
+//             }
+//             else suffixMax[i] = Math.max(prices[i], suffixMax[i+1]);
+//         }
+        
+//         int maxProfit = 0;
+        
+//         for (int i = 0; i < len ; i++) {
+//             maxProfit = Math.max(maxProfit, suffixMax[i] - prefixMin[i]);
+//         }
+        
+//         return maxProfit;
     }
 }
